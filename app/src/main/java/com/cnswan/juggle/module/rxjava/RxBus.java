f7a1ac419c00d907050d1d6122f5b1b93bd7c49c
wan.chen <wan.chen@cimc.com>
2017-09-25 13:56:34 +0800

    upgrade rxbus

--- app/src/main/java/com/cnswan/juggle/module/rxjava/RxBus.java
+++ app/src/main/java/com/cnswan/juggle/module/rxjava/RxBus.java
@@ -1,9 +1,6 @@
 package com.cnswan.juggle.module.rxjava;
 
 import io.reactivex.Flowable;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
 import io.reactivex.processors.FlowableProcessor;
 import io.reactivex.processors.PublishProcessor;
 import io.reactivex.subscribers.SerializedSubscriber;
@@ -13,8 +10,6 @@ public class RxBus {
     //相当于Rxjava1.x中的Subject
     private final FlowableProcessor<Object> mBus;
     private static volatile RxBus sRxBus = null;
-
-    private static volatile RxBus mDefaultInstance;
 
     private RxBus() {
         //调用toSerialized()方法，保证线程安全
@@ -40,37 +35,6 @@ public class RxBus {
         new SerializedSubscriber<>(mBus).onNext(new RxBusBaseMessage(code, o));
     }
 
-
-    /**
-     * ofType操作符只发射指定类型的数据，这里指定了接受的类型是:RxBusBaseMessage.class;其内部就是filter+cast
-     * 根据传递的code和 eventType 类型返回特定类型(eventType)的 被观察者
-     * 对于注册了code为0，class为voidMessage的观察者，那么就接收不到code为0之外的voidMessage。
-     *
-     * @param code      事件code
-     * @param eventType 事件类型,首先进行一次检测,看你发来的类型是不是RxBusBaseMessage类型的;
-     * @param <T>
-     * @return 我不明白为什么使用了ofType过滤之后还要使用filter过滤;
-     * 使用map,将传入的RxBusBaseMessage有转化为对应的object;
-     * 使用cast:做强制类型转换,cast(Class),参数为class类型,这里是做安全监测,如果不能转化为制定的class文件,那么将直接跑出异常;
-     * <p>
-     * 操作符中的ReBusMessage参数是从
-     */
-    public <T> Flowable<T> toObservable(final int code, final Class<T> eventType) {
-        return mBus.ofType(RxBusBaseMessage.class)
-                .filter(new Predicate<RxBusBaseMessage>() {
-                    @Override
-                    public boolean test(@NonNull RxBusBaseMessage msg) throws Exception {
-                        //过滤code和eventType都相同(类型)的事件
-                        return msg.getCode() == code && eventType.isInstance(msg.getObject());
-                    }
-                }).map(new Function<RxBusBaseMessage, Object>() {
-                    @Override
-                    public Object apply(@NonNull RxBusBaseMessage msg) throws Exception {
-                        return msg.getObject();
-                    }
-                }).cast(eventType);
-    }
-
     public boolean hasSubscribers() {
         return mBus.hasSubscribers();
     }
@@ -78,9 +42,6 @@ public class RxBus {
 
 /*
 1、首先创建一个可同时充当Observer和Observable的Subject；
-
 2、在需要接收事件的地方，订阅该Subject（此时Subject是作为Observable），在这之后，一旦Subject接收到事件，立即发射给该订阅者；
-
 3、在我们需要发送事件的地方，将事件post至Subject，此时Subject作为Observer接收到事件（onNext），然后会发射给所有订阅该Subject的订阅者。
-
  */
--- app/src/main/java/com/cnswan/juggle/module/rxjava/RxBusBaseMessage.java
+++ app/src/main/java/com/cnswan/juggle/module/rxjava/RxBusBaseMessage.java
@@ -1,17 +1,17 @@
 package com.cnswan.juggle.module.rxjava;
 
-/**
- * Created by zhangxin on 16/5/17.
- * 用来发送消息的RxBusMessage,只有被观察者发出这个类型的消息后,观察者才能做出响应;
- */
 public class RxBusBaseMessage {
-    private int code;
+
+    private int    code;
     private Object object;
-    public RxBusBaseMessage(int code, Object object){
-        this.code=code;
-        this.object=object;
+
+    public RxBusBaseMessage(int code, Object object) {
+        this.code = code;
+        this.object = object;
     }
-    public RxBusBaseMessage(){}
+
+    public RxBusBaseMessage() {
+    }
 
     public int getCode() {
         return code;
